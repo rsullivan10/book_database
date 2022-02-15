0699efb2242a8986d3a9ea9b641427d883360759
Roger Sullivan <sullivanrogerl@gmail.com>
2022-02-15 17:49:56 -0600

    added database modle

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+from models import Base, session, Book, engine
+
 # create a database 
 # books.db
 # create a modle
 # title , author, date published, price
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    
--- models.py
+++ models.py
@@ -1,4 +1,27 @@
-# import models
+from sqlalchemy import create_engine, String, Integer, Column, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///books.db',echo = False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+    
+    id = Column(Integer, primary_key=True)
+    title = Column('Title', String)
+    author = Column('Author', String)
+    published_date = Column('Published Date', Date)
+    price =  Column('Price', Integer)
+    
+    def __repr__(self):
+        return  f'Title: {self.title}, Author: {self.author}, Publish Date: {self.published_date}, Price: {self.price}'
+    
+
+
+
 # main menu - add, search, analysis, exit, view
 # add books to the database
 # edit books
