c44960e6fdeaa40ac2f5de97b873b635ab5310a7
Roger Sullivan <sullivanrogerl@gmail.com>
2022-02-17 09:29:52 -0600

    added CSV to DB

--- app.py
+++ app.py
@@ -1,10 +1,78 @@
 from models import Base, session, Book, engine
+import datetime
+import csv
 
-# create a database 
-# books.db
-# create a modle
-# title , author, date published, price
+def clean_date(date_str):
+    months = ['January', 'February', 'March', 'April', 'May', 
+        'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    split_date = date_str.split(' ')
+    month = int(split_date.index(split_date[0]) + 1)
+    day = int(split_date[1].split(',')[0])
+    year = int(split_date[2])
+    return datetime.date(year,month,day)
+
+def clean_price(price_str):
+    price_float = float(price_str)
+    return int(price_float * 100)
+
+def add_csv():
+    with open('suggested_books.csv') as csvfile:
+        data = csv.reader(csvfile)
+        for row in data:
+            book_in_db = session.query(Book).filter(Book.title==row[0]).one_or_none()
+            if book_in_db == None:
+                title = row[0]
+                author = row[1]
+                date = clean_date(row[2])
+                price = clean_price(row[3])
+                new_book = Book(title = title,author = author, published_date = date, price = price)
+                session.add(new_book)
+        session.commit()
+    
+def menu():
+    while True:
+        print('''
+              \nPROGRAMMING BOOKS
+              \r1) Add book
+              \r2) View all books
+              \r3) Search for book
+              \r4) Book Analaysis
+              \r5) Exit
+              ''')
+        choice = input('What would you like to do?: ')
+        if choice in ['1','2','3','4','5']:
+            return choice
+        else:
+            input('''
+                  \nplease choose one of the options above,
+                  \ra number from 1-5.
+                  \rPress enter to try again.
+                  ''')
+def app():
+    
+    app_running = True
+    while app_running:
+        choice = menu()
+        
+        if choice == '1':
+            #add book
+            pass
+        elif choice == '2':
+            #View all books
+            pass
+        elif choice == '3':
+            #Search for book
+            pass
+        elif choice == '4':
+            #Book Analaysis
+            pass
+        else: 
+            print('GOODBYE')
+            app_running = False
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    
+    # app()
+    add_csv()
+    for book in session.query(Book):
+        print(book)
